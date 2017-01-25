662b844312b13fa2d5550cfdf61830d5fa465523
Tim Schindler <xh3b4sd@gmail.com>
2017-01-25 14:01:27 +0100

    changed variable names

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,12 +3,11 @@
 set -eu
 
 NETWORK_BRIDGE_IP=$(/sbin/ifconfig ${NETWORK_BRIDGE_NAME} | grep 'inet ' | awk '{print $2}')
+echo "Using network bridge IP '${NETWORK_BRIDGE_IP}'."
 
-echo "Using network bridge IP '${NETWORK_BRIDGE_IP}'"
-
-cat << EOF > /tmp/bridge-ip-configmap-${SUFFIX_CONFIGMAP}.json
-{"apiVersion": "v1","kind": "ConfigMap","metadata": {"name": "bridge-ip-configmap-${SUFFIX_CONFIGMAP}", "namespace": "${K8S_NAMESPACE}"},"data": {"bridge-ip": "${NETWORK_BRIDGE_IP}"}}
+cat << EOF > ${BRIDGE_IP_CONFIGMAP_PATH}
+{"apiVersion": "v1","kind": "ConfigMap","metadata": {"name": "${BRIDGE_IP_CONFIGMAP_NAME}", "namespace": "${K8S_NAMESPACE}"},"data": {"bridge-ip": "${NETWORK_BRIDGE_IP}"}}
 EOF
 
-echo "Generated config map for network bridge IP"
-cat /tmp/bridge-ip-configmap-${SUFFIX_CONFIGMAP}.json
+echo "Generated config map for network bridge IP."
+cat ${BRIDGE_IP_CONFIGMAP_PATH}

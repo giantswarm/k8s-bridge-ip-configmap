bf69d1dd17c78f83eccd4e5149cfa05032bd14a3
Tim Schindler <xh3b4sd@gmail.com>
2017-02-09 01:14:47 +0100

    fixed bridge IP scanning (#2)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,19 @@
 
 set -eu
 
-NETWORK_BRIDGE_IP=$(/sbin/ifconfig ${NETWORK_BRIDGE_NAME} | grep 'inet ' | awk '{print $2}')
+NETWORK_BRIDGE_IP=""
+
+while :; do
+  NETWORK_BRIDGE_IP=$(/sbin/ifconfig ${NETWORK_BRIDGE_NAME} | grep 'inet ' | awk '{print $2}' | cut -d ':' -f 2)
+  if [ ! -z "${NETWORK_BRIDGE_IP}" ]; then
+    break
+  fi
+  sleep 1
+done
+
 echo "Using network bridge IP '${NETWORK_BRIDGE_IP}'."
 
-cat << EOF > ${BRIDGE_IP_CONFIGMAP_PATH}
+cat <<EOF >${BRIDGE_IP_CONFIGMAP_PATH}
 {"apiVersion": "v1","kind": "ConfigMap","metadata": {"name": "${BRIDGE_IP_CONFIGMAP_NAME}", "namespace": "${K8S_NAMESPACE}"},"data": {"bridge-ip": "${NETWORK_BRIDGE_IP}"}}
 EOF
 
